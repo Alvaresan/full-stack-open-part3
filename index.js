1e93623ece734bb6aa46a223ef03dfcb16bc83ae
Alvaresan <alvaresan@icloud.com>
2024-06-25 21:08:39 +0200

    Phonebook backend step 3

--- index.js
+++ index.js
@@ -35,6 +35,18 @@ app.get("/info", (request, response) => {
   `);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  const person = persons.find((person) => person.id === Number(id));
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT);
 
