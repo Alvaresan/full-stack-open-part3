f38339ae45f998bd3fc2b65ccdb4454ede674343
Alvaresan <alvaresan@icloud.com>
2024-06-26 11:18:45 +0200

    Phonebook backend step 7 and 8

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
-app.use(express.json());
+morgan.token("body", (req) => JSON.stringify(req.body));
 
 let persons = [
   {
@@ -25,6 +26,11 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
@@ -70,7 +76,7 @@ app.post("/api/persons", (request, response) => {
       error: "name missing",
     });
   }
-  
+
   if (!body.number) {
     return response.status(400).json({
       error: "number missing",
@@ -92,6 +98,6 @@ app.post("/api/persons", (request, response) => {
 });
 
 const PORT = 3001;
-app.listen(PORT);
-
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
